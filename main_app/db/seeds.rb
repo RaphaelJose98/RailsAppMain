a8acb14a60f9293d9cd6c94aedbd3138cb5b46d3
TRUECALLER <truecaller@TRUECALLERs-MacBook-Air-3.local>
2019-06-01 11:05:50 +0530

    micropost feauture

--- main_app/db/seeds.rb
+++ main_app/db/seeds.rb
@@ -26,3 +26,9 @@ User.create!(name:  "Admin",
                activated: true,
                activated_at: Time.zone.now)
 end
+
+users = User.order(:created_at).take(6)
+35.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
